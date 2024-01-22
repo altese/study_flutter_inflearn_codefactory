4941347cf0d637407fb32dd16c1bd11bf0a3fb83
altese <readme42u@gmail.com>
2024-01-22 22:31:56 +0900

    🔨 [API] auth/login, auth/token 버튼에 요청 연결

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:inflearn_code_factory/common/component/custom_text_form_field.dart';
 import 'package:inflearn_code_factory/common/const/colors.dart';
@@ -8,6 +12,14 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Dio dio = Dio();
+
+    // localhost
+    const emulatorIp = '10.0.2.2:3000';
+    const simulatorIp = '127.0.0.1:3000';
+
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
     return DefaultLayout(
       backgroundColor: const Color(0xFFd5f3ef),
       // 키보드가 올라올 때 오류 나지 않도록 SingleChildScrollView로 감싼다.
@@ -61,7 +73,26 @@ class LoginScreen extends StatelessWidget {
                                     BorderRadius.all(Radius.circular(15)),
                               ),
                             ),
-                            onPressed: () {},
+                            onPressed: () async {
+                              // refresh token, access token을 받아오기
+                              // ID:비밀번호
+                              const rawString = 'test@codefactory.ai:testtest';
+
+                              // Base64로 인코딩하는 코드
+                              Codec<String, String> stringToBase64 =
+                                  utf8.fuse(base64);
+                              String token = stringToBase64.encode(rawString);
+
+                              final response = await dio.post(
+                                'http://$ip/auth/login',
+                                options: Options(
+                                  headers: {'authorization': 'Basic $token'},
+                                ),
+                              );
+
+                              // response의 body가 response.data
+                              print(response.data);
+                            },
                             child: const Text(
                               '로그인',
                               style: TextStyle(
@@ -75,7 +106,20 @@ class LoginScreen extends StatelessWidget {
                         TextButton(
                           style: TextButton.styleFrom(
                               foregroundColor: PRIMARY_COLOR),
-                          onPressed: () {},
+                          onPressed: () async {
+                            // token refresh 하기
+                            final response = await dio.post(
+                              'http://$ip/auth/token',
+                              options: Options(
+                                headers: {
+                                  'authorization':
+                                      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTcwNTkyOTcyNywiZXhwIjoxNzA2MDE2MTI3fQ.SP2KACrpYgVB-jgtdOYxkOiyAYdRfuh_tVHJAcRhF7k'
+                                },
+                              ),
+                            );
+
+                            print(response.data);
+                          },
                           child: const Text(
                             '회원가입',
                             style: TextStyle(
