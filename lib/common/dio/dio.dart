83ff3ba8a6daeee746511b2139e8df7a2f69adc5
altese <readme42u@gmail.com>
2024-01-30 21:18:29 +0900

    🔨 Dio Intercetpor: onResponse 구현

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -41,6 +41,13 @@ class CustomInterceptor extends Interceptor {
   }
 
   // 2. 응답을 받을 때
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print(
+        '[RES] [${response.requestOptions.method}] ${response.requestOptions.uri}');
+
+    return super.onResponse(response, handler);
+  }
 
   // 3. 에러가 났을 때
   // 중요한 것: 어떤 상황을 catch하고 싶은가?
