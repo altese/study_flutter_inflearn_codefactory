cc6c3bd23c31e39b824125135c66983bef79884a
altese <readme42u@gmail.com>
2024-02-07 22:50:23 +0900

    🔨 CursorPaginationBase: 클래스로 상태 구분하기
    - CursorPaginationError: 에러가 발생했을 때
    - CursorPaginationLoading: 로딩 중일 때
    - CursorPaginationModel: 데이터가 잘 있을 때
    - CursorPaginationRefetching: 새로고침
    - CursorPaginationFetchingMore: 추가 데이터 로딩 중

--- lib/product/model/cursor_pagination_model.dart
+++ lib/product/model/cursor_pagination_model.dart
@@ -2,8 +2,27 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+// 클래스로 상태 구분하기: 무조건 Base로 만듦
+// - 아무 기능을 하지 않는 추상 클래스지만 CursorPaginationModel의 타입을 판단했을 때
+// CursorPaginationBase가 나온다는 점이 중요함.
+// - CursorPaginationModel은 데이터가 잘 있을 때
+// - CursorPaginationError는 에러가 났을 때
+// - CursorPaginationLoading은 로딩 중일 때
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+// 아무 구현 없어도 됨! CursorPaginationLoading 타입인 것만으로도 로딩 중이라는 의미니까
+class CursorPaginationLoading extends CursorPaginationBase {}
+
 @JsonSerializable(genericArgumentFactories: true)
-class CursorPaginationModel<T> {
+class CursorPaginationModel<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   // 리스트의 타입만 유동적으로 바뀐다. -> 제너릭으로 받아오는 것으로 해결
   final List<T> data;
@@ -31,4 +50,22 @@ class CursorPaginationMeta {
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+// 새로고침할 때
+// meta, data가 이미 존재한다는 가정 하에 사용하기 때문에 CursorPaginationModel 상속
+// CursorPaginationRefetching도 Base의 서브 클래스
+class CursorPaginationRefetching<T> extends CursorPaginationModel<T> {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+// 리스트의 맨 아래로 내려서 추가 데이터 요청할 때
+class CursorPaginationFetchingMore<T> extends CursorPaginationModel<T> {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inflearn_code_factory/product/model/cursor_pagination_model.dart';
 import 'package:inflearn_code_factory/restaurant/model/restaurant_model.dart';
 import 'package:inflearn_code_factory/restaurant/repository/restaurant_repository.dart';
 
@@ -12,11 +13,15 @@ final restaurantProvider =
   },
 );
 
-class RestaurantStateNotifier extends StateNotifier<List<RestaurantModel>> {
+// 현재 코드의 문제점
+// 페이지네이션을 하려면 CursorPaginationModel 안의 List<RestaurantModel>만 가지고 있을 게 아니라
+// meta 데이터도 가지고 있어야 hasMore을 확인하고 다음 요청을 보낼지 말지 판단할 수 있다.
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationModel> {
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository,
+    // super constructor 안에는 CursorPaginationModel가 아직 로딩 상태라는 값을 넣어 줘야 함
   }) : super([]) {
     // RestaurantStateNotifier가 생성되자마자 페이지네이션 요청을 해야 한다.
     // 값을 가지고 있어야 하기 때문에
