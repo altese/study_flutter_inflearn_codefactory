aff6a7230a00d31f4b4c7cb81f89c072bf55458e
altese <readme42u@gmail.com>
2024-02-07 23:13:13 +0900

    🔨 CursorPaginationBase 적용

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,10 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inflearn_code_factory/product/model/cursor_pagination_model.dart';
-import 'package:inflearn_code_factory/restaurant/model/restaurant_model.dart';
 import 'package:inflearn_code_factory/restaurant/repository/restaurant_repository.dart';
 
 final restaurantProvider =
-    StateNotifierProvider<RestaurantStateNotifier, List<RestaurantModel>>(
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
   (ref) {
     final repository = ref.watch(restaurantRepositoryProvider);
     final notifier = RestaurantStateNotifier(repository: repository);
@@ -16,13 +15,13 @@ final restaurantProvider =
 // 현재 코드의 문제점
 // 페이지네이션을 하려면 CursorPaginationModel 안의 List<RestaurantModel>만 가지고 있을 게 아니라
 // meta 데이터도 가지고 있어야 hasMore을 확인하고 다음 요청을 보낼지 말지 판단할 수 있다.
-class RestaurantStateNotifier extends StateNotifier<CursorPaginationModel> {
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository,
-    // super constructor 안에는 CursorPaginationModel가 아직 로딩 상태라는 값을 넣어 줘야 함
-  }) : super([]) {
+    //*** super constructor 안에는 CursorPaginationModel가 아직 로딩 상태라는 값을 넣어 줘야 함
+  }) : super(CursorPaginationLoading()) {
     // RestaurantStateNotifier가 생성되자마자 페이지네이션 요청을 해야 한다.
     // 값을 가지고 있어야 하기 때문에
     paginate();
@@ -31,6 +30,6 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationModel> {
   paginate() async {
     final resp = await repository.paginate();
 
-    state = resp.data;
+    state = resp;
   }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inflearn_code_factory/product/model/cursor_pagination_model.dart';
 import 'package:inflearn_code_factory/restaurant/component/restaurant_card.dart';
 import 'package:inflearn_code_factory/restaurant/provider/restaurant_provider.dart';
 import 'package:inflearn_code_factory/restaurant/view/restaurant_detail_screen.dart';
@@ -13,35 +14,37 @@ class RestaurantScreen extends ConsumerWidget {
     final data = ref.watch(restaurantProvider);
 
     // 임시 예외처리!: 개선 예정
-    if (data.isEmpty) {
+    if (data is CursorPaginationLoading) {
       return const Center(child: CircularProgressIndicator());
-    } else {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView.separated(
-          itemCount: data.length,
-          itemBuilder: (_, index) {
-            final parsedItem2 = data[index];
-
-            return GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => RestaurantDetailScreen(
-                      id: parsedItem2.id,
-                    ),
-                  ),
-                );
-              },
-              child: RestaurantCard.fromModel(model: parsedItem2),
-            );
-          },
-          // 아이템 사이에 들어가는 위젯
-          separatorBuilder: (_, index) {
-            return const SizedBox(height: 15);
-          },
-        ),
-      );
     }
+
+    final cp = data as CursorPaginationModel;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: ListView.separated(
+        itemCount: cp.data.length,
+        itemBuilder: (_, index) {
+          final parsedItem2 = cp.data[index];
+
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => RestaurantDetailScreen(
+                    id: parsedItem2.id,
+                  ),
+                ),
+              );
+            },
+            child: RestaurantCard.fromModel(model: parsedItem2),
+          );
+        },
+        // 아이템 사이에 들어가는 위젯
+        separatorBuilder: (_, index) {
+          return const SizedBox(height: 15);
+        },
+      ),
+    );
   }
 }
