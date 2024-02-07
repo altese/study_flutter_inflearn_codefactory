cf7ce32d5e26833c75585692585fae921ad86bcf
altese <readme42u@gmail.com>
2024-02-07 23:14:54 +0900

    🔨 [API /restaurant] 파라미터 추가
    - optional 파라미터인 after, count 모델을 만들어서

--- lib/restaurant/repository/restaurant_repository.dart
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inflearn_code_factory/common/const/data.dart';
 import 'package:inflearn_code_factory/common/dio/dio.dart';
 import 'package:inflearn_code_factory/product/model/cursor_pagination_model.dart';
+import 'package:inflearn_code_factory/product/model/pagination_params.dart';
 import 'package:inflearn_code_factory/restaurant/model/restaurant_detail_model.dart';
 import 'package:inflearn_code_factory/restaurant/model/restaurant_model.dart';
 import 'package:retrofit/retrofit.dart';
@@ -28,7 +29,10 @@ abstract class RestaurantRepository {
   @Headers({
     'accessToken': 'true',
   })
-  Future<CursorPaginationModel<RestaurantModel>> paginate();
+  Future<CursorPaginationModel<RestaurantModel>> paginate({
+    // 파라미터 설정
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
 
   @GET('/{id}')
   @Headers({
