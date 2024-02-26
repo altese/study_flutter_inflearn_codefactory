f00ae17b162daed5b272a7c106e26758f0cfc000
altese <readme42u@gmail.com>
2024-02-26 22:00:19 +0900

    🔨 [Pagination] 리스트뷰 하단까지 스크롤 시 데이터 추가 요청

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -5,24 +5,56 @@ import 'package:inflearn_code_factory/restaurant/component/restaurant_card.dart'
 import 'package:inflearn_code_factory/restaurant/provider/restaurant_provider.dart';
 import 'package:inflearn_code_factory/restaurant/view/restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 컨트롤러의 특정 값이 바뀔 때마다 scrollListener 실행
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이보다 조금 덜되는 위치라면 추가 요청
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // FutureBuilder 대체
     final data = ref.watch(restaurantProvider);
 
-    // 임시 예외처리!: 개선 예정
+    // 첫 로딩
     if (data is CursorPaginationLoading) {
       return const Center(child: CircularProgressIndicator());
     }
+
+    // 에러
+    if (data is CursorPaginationError) {
+      return Center(child: Text(data.message));
+    }
+
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetching
 
     final cp = data as CursorPaginationModel;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: ListView.separated(
+        controller: controller,
         itemCount: cp.data.length,
         itemBuilder: (_, index) {
           final parsedItem2 = cp.data[index];
