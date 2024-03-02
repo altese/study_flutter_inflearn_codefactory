4c0cbc79d89d6599f36a3a0c3183abb73010bd19
altese <readme42u@gmail.com>
2024-03-02 23:02:00 +0900

    🔨 [리뷰] 모델 생성
    - ratingModel, userModel

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,7 +1,12 @@
 import 'package:inflearn_code_factory/common/const/data.dart';
 
 class DataUtils {
-  static pathToUrl(String value) {
+  static String pathToUrl(String value) {
     return 'http://$ip$value';
+  }
+
+  // url을 리스트로 받았을 때
+  static List<String> listPathsToUrls(List<String> paths) {
+    return paths.map((e) => pathToUrl(e)).toList();
   }
 }
