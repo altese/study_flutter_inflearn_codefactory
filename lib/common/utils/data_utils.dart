9ffdef73f37664ab2c99e5d9dbc0a1c747598a76
altese <readme42u@gmail.com>
2024-03-21 21:35:20 +0900

    🔨 [AuthRepository] - /auth/login → login() - /auth/token → getToken()

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:inflearn_code_factory/common/const/data.dart';
 
 class DataUtils {
@@ -8,5 +10,13 @@ class DataUtils {
   // url을 리스트로 받았을 때
   static List<String> listPathsToUrls(List paths) {
     return paths.map((e) => pathToUrl(e)).toList();
+  }
+
+  // plain 텍스트를 Base64로 인코딩
+  static String plainToBase64(String plain) {
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+    String encoded = stringToBase64.encode(plain);
+
+    return encoded;
   }
 }
---
+++ lib/user/repository/auth_repository.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inflearn_code_factory/common/utils/data_utils.dart';
+import 'package:inflearn_code_factory/product/model/login_response.dart';
+import 'package:inflearn_code_factory/product/model/token_response.dart';
+
+class AuthRepository {
+  // 'http://$ip/auth'
+  final String baseUrl;
+  final Dio dio;
+
+  AuthRepository({
+    required this.baseUrl,
+    required this.dio,
+  });
+
+  Future<LoginResponse> login({
+    required String username,
+    required String password,
+  }) async {
+    final serialized = DataUtils.plainToBase64('$username : $password');
+
+    final resp = await dio.post(
+      '$baseUrl/login',
+      options: Options(
+        headers: {'authorization': 'Basic $serialized'},
+      ),
+    );
+
+    return LoginResponse.fromJson(resp.data);
+  }
+
+  Future<TokenResponse> getToken(WidgetRef ref) async {
+    final resp = await dio.post(
+      '$baseUrl/token',
+      options: Options(
+        // headers: {'authorization': 'Bearer $refreshToken'},
+        // interceptor에서 처리된다.
+        headers: {'refreshToken': 'true'},
+      ),
+    );
+
+    return TokenResponse.fromJson(resp.data);
+  }
+}
