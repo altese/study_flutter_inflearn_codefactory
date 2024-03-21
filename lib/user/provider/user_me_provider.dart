154c53b5868676f412d693e517a5747ca01cdb08
altese <readme42u@gmail.com>
2024-03-21 22:04:38 +0900

    🔨 [로그인, 로그아웃]
    - UserMeStateNotifier

--- lib/user/provider/user_me_provider.dart
+++ lib/user/provider/user_me_provider.dart
@@ -2,13 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:inflearn_code_factory/common/const/data.dart';
 import 'package:inflearn_code_factory/user/model/user_model.dart';
+import 'package:inflearn_code_factory/user/repository/auth_repository.dart';
 import 'package:inflearn_code_factory/user/repository/user_me_repository.dart';
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final AuthRepository authRepository;
   final UserMeRepository repository;
   final FlutterSecureStorage storage;
 
   UserMeStateNotifier({
+    required this.authRepository,
     required this.repository,
     required this.storage,
   }) : super(UserModelLoading()) {
@@ -29,5 +32,45 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
     final resp = await repository.getMe();
 
     state = resp;
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      // 일단 로딩
+      state = UserModelLoading();
+
+      // 토큰 받아와서 저장
+      final resp = await authRepository.login(
+        username: username,
+        password: password,
+      );
+
+      await storage.write(key: REFRESH_TOKEN_KEY, value: resp.refreshToken);
+      await storage.write(key: ACCESS_TOKEN_KEY, value: resp.accessToken);
+
+      // 사용자 정보 state에 저장
+      final userResp = await repository.getMe();
+
+      state = userResp;
+    } catch (e) {
+      state = UserModelError(message: '로그인에 실패했습니다.');
+    }
+    return Future.value(state);
+  }
+
+  Future<void> logOut() async {
+    // await storage.delete(key: REFRESH_TOKEN_KEY);
+    // await storage.delete(key: ACCESS_TOKEN_KEY);
+
+    // 동시에 지우고 싶을 때
+    await Future.wait([
+      storage.delete(key: REFRESH_TOKEN_KEY),
+      storage.delete(key: ACCESS_TOKEN_KEY),
+    ]);
+
+    state = null;
   }
 }
